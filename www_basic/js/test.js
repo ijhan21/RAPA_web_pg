0700f5a02b2937ef0b5571c4f27639a246515216
임종한 <withyou@empal.com>
2021-10-15 17:37:54 +0900

    make stars

--- www_basic/js/test.js
+++ www_basic/js/test.js
@@ -1,2 +1,20 @@
-const num = 5
-console.log('$num')
+const num =10
+let array = []
+for (let i=0; i<num;i++){
+    let output = ""
+    for (let j=0; j<num-i;j++){
+        output +=' . '
+        }
+    for (let j=0; j<=2*i;j++){
+    output +=' 8 '
+    }
+    for (let j=0; j<num-i;j++){
+        output +=' . '
+        }
+    console.log(output)
+    array.push(output)
+}
+for (ar in array){
+    if (ar==0){ continue}
+    console.log(array[array.length-1-ar])
+}
