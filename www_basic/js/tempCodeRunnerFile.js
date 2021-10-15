cc19c1aeefec1fa8d92bac984680d9a5b31b021f
임종한 <withyou@empal.com>
2021-10-15 16:53:27 +0900

    js codes

---
+++ www_basic/js/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+
+// const todos1 = ['출근', '고객미팅', 'js수강', '탁구']
+// for (const todo of todos1){
+// console.log(`오늘 할 일 : ${todo}`)
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(`${i}번째 반복입니다.`)
+//     }
+
+// let output = 0
+// for (let i=1; i<=100; i++){
+//     output +=i
+// }
+
+// console.log(output)
+// console.log(' 1~100까지 합 : ${output} ')
